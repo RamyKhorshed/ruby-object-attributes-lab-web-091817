2be39367eb2eb0f143766df7f470413cc93136a8
Ramy Khorshed <RamyKhorshed@gmail.com>
2017-09-20 10:49:08 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  #def initialize(name)
+  #  @name = name
+  #end
+
+  def name=(inputName)
+    @name = inputName
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(inputName)
+    @breed = inputName
+  end
+
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+  #def initialize(name)
+  #  @name = name
+  #end
+
+  def name=(inputName)
+    @name = inputName
+  end
+
+  def name
+    @name
+  end
+
+  def job=(inputName)
+    @job = inputName
+  end
+
+  def job
+    @job
+  end
+end
